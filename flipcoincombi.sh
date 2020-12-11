85d6f30ed12ddc43fecd2eb4f0f3019d236e97c1
sonal <sonalkarle01@gmail.com>
2020-12-12 02:09:18 +0530

    Added code todisplay Head and Tail

--- flipcoincombi.sh
+++ flipcoincombi.sh
@@ -1,2 +1,23 @@
 echo "Welcome"
+#!/usr/bin/bash -x
 
+function checkResult ()
+{
+	if [ $1 -eq 0 ]
+	then
+		echo "Heads"
+	else
+		echo "Tails"
+	fi
+}
+echo "Welcome To flipCoin simulator"
+
+
+flipTheCoin=y
+
+while [ $flipTheCoin == y ]
+do
+	random=$(( RANDOM % 2 ))
+	checkResult $random
+read -p "do you wish to flip coin again? (y/n): " flipTheCoin
+done
